09b51b347579eb2986f05198220d7f4da4cffdbc
Camila Murcia <mariamurmu@unisabana.edu.co>
2026-03-15 19:53:18 -0500

    p3 1 y 2

---
+++ function_types.py
@@ -0,0 +1,10 @@
+def list_shift(lista_flo, valor):
+    for num in range(len(lista_flo)):
+        lista_flo[num]=lista_flo[num] + valor
+
+def calc_avg(lista_float):
+    total = 0
+    for num in lista_float:
+        total= total + num
+        promedio = total / len(lista_float)
+        return promedio
