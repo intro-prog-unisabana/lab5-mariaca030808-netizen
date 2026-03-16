a4bb2287d08e7fedd627505a02e551de9e64a943
Camila Murcia <mariamurmu@unisabana.edu.co>
2026-03-15 20:12:46 -0500

    p6

---
+++ modules.py
@@ -0,0 +1,13 @@
+import os
+import math
+
+directorio = os.getcwd()
+print(f"Current working directory: {directorio}")
+num = int(input("Enter an integer:\n"))
+log_val = math.log2(num)
+print(f"Log base 2 of {num} is: {log_val}")
+floor_val = math.floor(log_val)
+ceil_val = math.ceil(log_val)
+
+print(f"Floor: {floor_val}")
+print(f"Ceiling: {ceil_val}")
---
+++ scope.py
@@ -0,0 +1,4 @@
+some_int= None
+some_str= None
+
+def set_globals(some_int, some_str):
