2c439fb7639b740645dc9ade9928cd940d52d434
Camila Murcia <mariamurmu@unisabana.edu.co>
2026-03-15 20:38:06 -0500

    caller p

--- caller.py
+++ caller.py
@@ -1,5 +1,9 @@
+from mystery_module import transform_data
+x= int(input())
+y= float(input())
+result = transform_data(x, y, "quiz_test")
 
-
+print(result)
 # FREEZE CODE BEGIN
 x = int(input())
 y = float(input())
