4f76fb857d0eb441166df2c1a59d7baf5e972f9a
Camila Murcia <mariamurmu@unisabana.edu.co>
2026-03-15 20:21:15 -0500

    scope

--- scope.py
+++ scope.py
@@ -1,4 +1,10 @@
-some_int= None
-some_str= None
+int_value = None
+str_value = None
 
 def set_globals(some_int, some_str):
+    global int_value
+    global str_value
+    int_value=some_int
+    str_value=some_str
+def get_globals():
+    return (int_value, str_value)
