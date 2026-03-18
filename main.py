7d5d4e0916e1780e20b260553088d730da4939f7
Camila Murcia <mariamurmu@unisabana.edu.co>
2026-03-17 21:31:25 -0500

    prueba calc

--- main.py
+++ main.py
@@ -11,35 +11,29 @@ while True:
         num2= float(input("Enter the second number:\n"))
         result= add(num1, num2)
         print("The result is:", result)
-
     elif operación == "subtract":
         num1= float(input("Enter the first number:\n"))
         num2= float(input("Enter the second number:\n"))
         result= sub(num1, num2)
         print("The result is:", result)
-
     elif operación == "multiply":
         num1= float(input("Enter the first number:\n"))
         num2= float(input("Enter the second number:\n"))
         result= multiply(num1, num2)
         print("The result is:", result)
-
     elif operación == "divide":
         num1= float(input("Enter the first number:\n"))
         num2= float(input("Enter the second number:\n"))
         result= divide(num1, num2)
-
         if isinstance(result, str):
             print(result)
         else:
             print("The result is:", result)
-
     elif operación == "exponent":
         num1= float(input("Enter the first number:\n"))
         num2= float(input("Enter the second number:\n"))
         result= exponent(num1, num2)
         print("The result is:", result)
-
     elif operación == "modulo":
         num1= float(input("Enter the first number:\n"))
         num2= float(input("Enter the second number:\n"))
@@ -48,7 +42,6 @@ while True:
             print(result)
         else:
             print("The result is:", result)
-
     elif operación == "floor_divide":
         num1= float(input("Enter the first number:\n"))
         num2= float(input("Enter the second number:\n"))
@@ -57,11 +50,9 @@ while True:
             print(result)
         else:
             print("The result is:", result)
-
     elif operación == "absolute":
         num= float(input("Enter the number:\n"))
         result= absolute(num)
         print("The result is:", result)
-
     else:
         print("Invalid option!")
